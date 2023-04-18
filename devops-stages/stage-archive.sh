b4c4d4834f12c2340febd91d66946e94600075a8
garciadeblas <gerardo.garciadeblas@telefonica.com>
2023-04-18 14:53:02 +0200

    Clean stage-archive.sh
    Change-Id: Ib42096edb6bcbb29031749291fe5de410cb93755
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>

--- devops-stages/stage-archive.sh
+++ devops-stages/stage-archive.sh
@@ -21,8 +21,4 @@ rm -rf pool
 rm -rf dists
 mkdir -p pool/$MDG
 mv deb_dist/*.deb pool/$MDG/
-mkdir -p dists/unstable/$MDG/binary-amd64/
-apt-ftparchive packages pool/$MDG > dists/unstable/$MDG/binary-amd64/Packages
-gzip -9fk dists/unstable/$MDG/binary-amd64/Packages
-echo "dists/**,pool/$MDG/*.deb"
 
